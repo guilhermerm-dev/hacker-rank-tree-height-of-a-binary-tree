74b53b46edf7dd91bb71e894a915ab5bfff0c114
Guilherme Ramos Mendes <guilhermerm.ciandt@sulamerica.com.br>
2020-12-10 09:43:11 -0300

    Solution

---
+++ hacker-rank-tree-height-of-a-binary-tree/Node.cs
@@ -0,0 +1,26 @@
+﻿namespace hacker_rank_tree_height_of_a_binary_tree
+{
+    public class Node
+    {
+        public Node Left { get; private set; }
+
+        public Node Right { get; private set; }
+
+        public int Data { get; private set; }
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+
+        public void SetLeftNode(Node node)
+        {
+            Left = node;
+        }
+
+        public void SetRightNode(Node node)
+        {
+            Right = node;
+        }
+    }
+}
---
+++ hacker-rank-tree-height-of-a-binary-tree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace hacker_rank_tree_height_of_a_binary_tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int element = int.Parse(Console.ReadLine());
+            Node root = null;
+            while (element-- > 0)
+            {
+                int data = int.Parse(Console.ReadLine());
+                root = Insert(root, data);
+            }
+            int height = Height(root);
+            Console.ReadKey();
+        }
+
+        private static Node Insert(Node root, int data)
+        {
+            if (root is null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node currentNode;
+                if (data <= root.Data)
+                {
+                    currentNode = Insert(root.Left, data);
+                    root.SetLeftNode(currentNode);
+                }
+                else
+                {
+                    currentNode = Insert(root.Right, data);
+                    root.SetRightNode(currentNode);
+                }
+                return root;
+            }
+
+        }
+
+        private static int Height(Node node)
+        {
+            if (node is null || node.Left is null && node.Right is null)
+            {
+                return 0;
+            }
+            else
+            {
+                return (Math.Max(Height(node.Left), Height(node.Right)) + 1);
+            }
+        }
+    }
+}
